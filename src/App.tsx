d8e9e1598b4760701c0766e8b829b03b269d826e
Abdelrady <ahmedmc95@gmail.com.com>
2024-06-29 04:35:24 +0300

    add localization

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Footer from "./components/App/Footer";
 import '@mantine/core/styles.css';
 import AddTodoForm from "./components/AddTodoForm";
 import FloatIndicator from "./components/ui/FloatingIndicator";
+import ToggleTheme from "./components/App/toggleTheme/ToggleTheme";
 
 
 function App() {
@@ -10,14 +11,17 @@ function App() {
     <div className="flex flex-col items-center justify-center w-screen">
       <div className="container mx-auto flex flex-col space-y-8 justify-center items-center lg:w-1/2 mt-28">
         <MantineProvider defaultColorScheme="dark" cssVariablesSelector="html" withCssVariables={false} deduplicateCssVariables={false} withStaticClasses={false} withGlobalClasses={false} >
+          <ToggleTheme/>
           <FloatIndicator/>
-         <AddTodoForm/> 
+          <AddTodoForm/> 
+          <Footer />
         </MantineProvider>
+        
       </div>
-      <Footer />
     </div>
   );
 }
 
 export default App
+
 
--- src/components/App/Footer/index.tsx
+++ src/components/App/Footer/index.tsx
@@ -1,17 +1,18 @@
 import { transFrom } from "@mongez/localization";
 import useLanguageStore from "../../../store/zustand/useLanguageStore";
+import { Text } from "@mantine/core";
 
 export default function Footer() {
   const lang = useLanguageStore((state) => state.lang);
 
   return (
-    <div className="text-center text-slate-300 mt-80">
-      <p className="text-xs">
+    <div className="text-center  mt-80">
+      <Text size="sm" >
         © {new Date().getFullYear()} 
         {' '}
         {transFrom(lang, "copyrights")}
-      </p>
-      <p className="text-xs">
+      </Text>
+      <Text size="sm" >
         {transFrom(lang, "developedby")}
         {' '}
         <a
@@ -21,7 +22,7 @@ export default function Footer() {
         >
           {transFrom(lang, "author")}
         </a>
-      </p>
+      </Text>
     </div>
   );
 }
